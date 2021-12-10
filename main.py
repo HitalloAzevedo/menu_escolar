6521e92f6d452a07a7e616f0a25146a654fc678a
HitalloAzevedo <azevedohitallo@gmail.com>
2021-12-10 12:57:39 -0300

    Update main.py
    mudanças gerais, criando toda interface principal

--- main.py
+++ main.py
@@ -6,16 +6,78 @@ author = 'Hitallo Azevedo'
 version = 'v0.0.1'
 # Muito obrigado pela preferência Github: @HitalloAzevedo
 
-cabecalho(f'MENU ESCOLAR {version}')
-print(f'Criado por {author}')
-print('=' * 60)
+def main():
+    while True:
+        cabecalho(f'MENU ESCOLAR {version}')
+        print(f'Criado por {author}')
+        print('=' * 60)
 
-opcao = menu('Progressão Aritmética',
-'Progressão Geométrica',
-'Fibonacci',
-'Função Primeiro Grau',
-'Função Segundo Grau',
-'Logaritmo')
+        opcao = menu('Progressão Aritmética',
+        'Progressão Geométrica',
+        'Fibonacci',
+        'Função Primeiro Grau',
+        'Função Segundo Grau',
+        'Logaritmo')
 
-print(opcao)
 
+
+        if opcao == 99:
+            print('Obrigado por usar!')
+            exit()
+
+        elif opcao == 1:
+            PA()
+            while True:
+                r = resposta()
+                if r == 'n':
+                    main()
+                else:
+                    PA()
+
+        elif opcao == 2:
+            PG()
+            while True:
+                r = resposta()
+                if r == 'n':
+                    main()
+                else:
+                    PG()
+
+        elif opcao == 3:
+            fibonacci()
+            while True:
+                r = resposta()
+                if r == 'n':
+                    main()
+                else:
+                    fibonacci()
+
+        elif opcao == 4:
+            funcao_primeiro()
+            while True:
+                r = resposta()
+                if r == 'n':
+                    main()
+                else:
+                    funcao_primeiro()
+
+        elif opcao == 5:
+            funcao_segundo()
+            while True:
+                r = resposta()
+                if r == 'n':
+                    main()
+                else:
+                    funcao_segundo()
+
+        elif opcao == 6:
+            logaritmo()
+            while True:
+                r = resposta()
+                if r == 'n':
+                    main()
+                else:
+                    logaritmo()
+
+if __name__ == '__main__':
+    main()
