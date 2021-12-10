b077f555ef49f33ab0e9cc493c878e1e6d7219d7
HitalloAzevedo <azevedohitallo@gmail.com>
2021-12-10 11:38:30 -0300

    mudança no main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#__author__ Hitallo Azevedo
+#__version__ v0.0.1
+# Muito obrigado pela preferência Github: @HitalloAzevedo
+
+from math import log
 from interface import *
 from calculos import *
 
 
-PA()
+logaritmo()
+
